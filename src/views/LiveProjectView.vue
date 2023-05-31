7eb0ddb03455989849b16e5ddfaa4c2c4d2b1fa6
izaheer <izaheer@teresol.com>
2023-05-31 16:08:48 +0500

    portfolio

---
+++ src/views/LiveProjectView.vue
@@ -0,0 +1,17 @@
+<template>
+  <p class="text-center mb-3 dark:text-blue-300">Demo for Projects</p>
+  <div class="max-h-[50vh] overflow-auto sm:w-4/12 mx-auto">
+    <!-- <a :href="project.html_url" target="_bl"> -->
+    <a target="_blank" class="font-bold text-xl" href="https://classy-cocada-91d8ff.netlify.app">
+      <div class="bg-blue-300 mb-4 p-2 rounded-md shadow-md">1. Management System</div>
+    </a>
+
+    <a class="font-bold text-xl" href="https://majestic-truffle-8ff96f.netlify.app" target="_blank">
+      <div class="bg-blue-300 mb-4 p-2 rounded-md shadow-md">2. Shopping Cart</div>
+    </a>
+
+    <!-- </a> -->
+  </div>
+</template>
+<script setup>
+</script>
