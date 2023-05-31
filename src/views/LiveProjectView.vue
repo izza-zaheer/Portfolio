b14408f5bcdef277ba7f6b5792d364c0d3c5014d
izaheer <izaheer@teresol.com>
2023-05-31 16:21:43 +0500

    portfolio

--- src/views/LiveProjectView.vue
+++ src/views/LiveProjectView.vue
@@ -9,6 +9,9 @@
     <a class="font-bold text-xl" href="https://majestic-truffle-8ff96f.netlify.app" target="_blank">
       <div class="bg-blue-300 mb-4 p-2 rounded-md shadow-md">2. Shopping Cart</div>
     </a>
+    <a class="font-bold text-xl" href="https://symphonious-piroshki-e41c85.netlify.app" target="_blank">
+      <div class="bg-blue-300 mb-4 p-2 rounded-md shadow-md">2. Portfolio</div>
+    </a>
 
     <!-- </a> -->
   </div>
