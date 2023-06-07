92013592269e67d538331e004a4d6ba386625750
izaheer <izaheer@teresol.com>
2023-06-07 15:45:03 +0500

    new project added

--- src/views/LiveProjectView.vue
+++ src/views/LiveProjectView.vue
@@ -1,19 +1,28 @@
 <template>
-  <p class="text-center mb-3 dark:text-blue-300">Demo for Projects</p>
-  <div class="max-h-[50vh] overflow-auto sm:w-4/12 mx-auto">
-    <!-- <a :href="project.html_url" target="_bl"> -->
-    <a target="_blank" class="font-bold text-xl" href="https://classy-cocada-91d8ff.netlify.app">
-      <div class="bg-blue-300 mb-4 p-2 rounded-md shadow-md">1. Management System</div>
-    </a>
+  <div>
+    <p class="text-center mb-3 dark:text-blue-300">Demo for Projects</p>
+    <div class="max-h-[50vh] overflow-auto sm:w-4/12 mx-auto">
+      <!-- <a :href="project.html_url" target="_bl"> -->
+        <a target="_blank" class="font-bold text-xl" href="https://fluid-simulations.netlify.app">
+        <div class="bg-blue-300 mb-4 p-2 rounded-md shadow-md">1. Fluid Simulation</div>
+      </a>
+      <a target="_blank" class="font-bold text-xl" href="https://classy-cocada-91d8ff.netlify.app">
+        <div class="bg-blue-300 mb-4 p-2 rounded-md shadow-md">2. Management System</div>
+      </a>
 
-    <a class="font-bold text-xl" href="https://majestic-truffle-8ff96f.netlify.app" target="_blank">
-      <div class="bg-blue-300 mb-4 p-2 rounded-md shadow-md">2. Shopping Cart</div>
-    </a>
-    <a class="font-bold text-xl" href="izza-zaheer-porfolio.netlify.app" target="_blank">
-      <div class="bg-blue-300 mb-4 p-2 rounded-md shadow-md">3. Portfolio</div>
-    </a>
+      <a
+        class="font-bold text-xl"
+        href="https://majestic-truffle-8ff96f.netlify.app"
+        target="_blank"
+      >
+        <div class="bg-blue-300 mb-4 p-2 rounded-md shadow-md">3. Shopping Cart</div>
+      </a>
+      <a class="font-bold text-xl" href="https://izza-zaheer-porfolio.netlify.app" target="_blank">
+        <div class="bg-blue-300 mb-4 p-2 rounded-md shadow-md">4. Portfolio</div>
+      </a>
 
-    <!-- </a> -->
+      <!-- </a> -->
+    </div>
   </div>
 </template>
 <script setup>
